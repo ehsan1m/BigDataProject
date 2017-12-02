aa77a89a7ee91a3c59499036c95de75b3c8ee817
Jemd13 <jemd311@gmail.com>
2017-12-02 14:01:36 -0800

    Pushing final long test

--- code/scikitEnsemble.py
+++ code/scikitEnsemble.py
@@ -23,20 +23,20 @@ assert spark.version >= '2.2'  # make sure we have Spark 2.2+
 # Generates a hyperparameter RDD with the indicated parameters
 def generateHyperParamsRDD() :
 	# Use this for long tests
-	# activationFuncs = ['logistic']
-	# learnRates = [0.2,0.1,0.05,0.02,0.01,0.005] # Learning Rates
-	# maxIters = [1000,1500] # Max number of epochs
-	# numHiddenL = [1,2,3] # Number of hidden layers
-	# neuronsPerLayer = [5,10,20] # Number of neurons in each hidden layer
-	# hiddenLayerNums = []
+	activationFuncs = ['logistic']
+	learnRates = [0.2,0.1,0.05,0.02,0.01,0.005] # Learning Rates
+	maxIters = [1000,1500] # Max number of epochs
+	numHiddenL = [1,2,3] # Number of hidden layers
+	neuronsPerLayer = [5,10,20] # Number of neurons in each hidden layer
+	hiddenLayerNums = []
 
 	# Use this for short tests
-	activationFuncs = ['logistic']
-	learnRates = [0.5,0.2,0.1,0.05,0.02] # Learning Rates
-	maxIters = [500,1000] # Max number of epochs
-	numHiddenL = [1] # Number of hidden layers
-	neuronsPerLayer = [1,2,5] # Number of neurons in each hidden layer
-	hiddenLayerNums = []
+	# activationFuncs = ['logistic']
+	# learnRates = [0.5,0.2,0.1,0.05,0.02] # Learning Rates
+	# maxIters = [500,1000] # Max number of epochs
+	# numHiddenL = [1] # Number of hidden layers
+	# neuronsPerLayer = [1,2,5] # Number of neurons in each hidden layer
+	# hiddenLayerNums = []
 
 	# Fill in the different hidden layer neuron combinations
 	for num in numHiddenL :
